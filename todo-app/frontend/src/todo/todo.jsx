5b87d2fb65068dba3d3faa1d143266cf9471d800
Gabriel Magalhães dos Santos <gabriel.wolfwood@gmail.com>
2018-03-27 19:34:27 -0300

    5/55 - Consulta e Exclusão de TODOs

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -16,18 +16,34 @@ export default class Todo extends Component {
       list:[]
     }
 
+    this.refresh = this.refresh.bind(this)
     this.handleAdd = this.handleAdd.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
+
+    this.refresh()
+  }
+
+  refresh() {
+    axios.get(`${URL}?sort=-createAt`).then(resp => {
+      this.setState({...this.state, description: '', list: resp.data})
+    })
   }
 
   handleAdd() {
     const description = this.state.description
 
-    axios.post(URL, { description }).then(resp => console.log('funcionou!'))
+    axios.post(URL, { description }).then(resp => {
+      this.refresh()
+    })
   }
 
   handleChange(e) {
     this.setState({...this.state, description: e.target.value})
+  }
+
+  handleRemove(todo) {
+    axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh())
   }
 
   render() {
@@ -38,7 +54,9 @@ export default class Todo extends Component {
           description={this.state.description} 
           handleAdd={this.handleAdd}
           handleChange={this.handleChange} />
-        <TodoList />
+        <TodoList 
+          list={this.state.list}
+          handleRemove={this.handleRemove} />
       </div>
     )
   }
