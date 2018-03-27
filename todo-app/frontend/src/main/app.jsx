96a162613d5caea22648feadcc404ee081ddfc28
Gabriel Magalhães dos Santos <gabriel.wolfwood@gmail.com>
2018-03-27 18:02:45 -0300

    5/45 - Configurando as Rotas (React-Router)

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -3,14 +3,12 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
 
-import Todo from '../todo/todo'
-import About from '../about/about'
+import Routes from './routes'
 import Menun from '../template/menu'
 
 export default props => (
   <div className='container'>
     <Menun />
-    <Todo />
-    <About />
+    <Routes />
   </div>
 )
