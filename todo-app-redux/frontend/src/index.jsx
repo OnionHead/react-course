394fd7f3f4e8fba27a4fd09844d53dfb1f19cf2b
Gabriel Magalhães dos Santos <gabriel.wolfwood@gmail.com>
2018-04-02 19:15:10 -0300

    7/78 - Aplicando Middleware (redux-multi)

--- todo-app-redux/frontend/src/index.jsx
+++ todo-app-redux/frontend/src/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
+
 import promise from 'redux-promise'
+import multi from 'redux-multi'
 
 import App from './main/app'
 import reducers from './main/reducers'
 
-const store = applyMiddleware(promise)(createStore)(reducers)
+const store = applyMiddleware(multi, promise)(createStore)(reducers)
 
 ReactDom.render(
   <Provider store={store}>
