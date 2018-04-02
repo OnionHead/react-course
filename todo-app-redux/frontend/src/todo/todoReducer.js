f9beebaa65938e97a0b2e4bc53640514451c8d5f
Gabriel Magalhães dos Santos <gabriel.wolfwood@gmail.com>
2018-04-02 18:38:33 -0300

    7/75 - Action Creator de Pesquisa

--- todo-app-redux/frontend/src/todo/todoReducer.js
+++ todo-app-redux/frontend/src/todo/todoReducer.js
@@ -22,6 +22,11 @@ export default (state = INITIAL_STATE, action) => {
         ...state,
         description: action.payload
       }
+    case 'TODO_SEARCHED':
+      return {
+        ...state,
+        list: action.payload.data
+      }
     default:
       return state
   }
