bc81cb7d6b126d19074891acaff64ee6c42e628b
Gabriel Magalhães dos Santos <gabriel.wolfwood@gmail.com>
2018-04-02 20:24:36 -0300

    7/82 - Limpar Formulário

--- todo-app-redux/frontend/src/todo/todoForm.jsx
+++ todo-app-redux/frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { changeDescription, search, add } from './todoActions'
+import { changeDescription, search, add, clear } from './todoActions'
 
 class TodoForm extends Component {
   constructor(props){
@@ -14,12 +14,12 @@ class TodoForm extends Component {
   }
 
   keyHandler(e) {
-    const { add, search, description } = this.props
+    const { add, search, description, clear } = this.props
 
     if(e.key === 'Enter'){
       e.shiftKey ? seach() : add(description)
     }else if(e.key === 'Escape'){
-      this.props.handleClear()
+      clear()
     }
   }
 
@@ -28,7 +28,7 @@ class TodoForm extends Component {
   }
 
   render() {
-    const { add, search, description, changeDescription } = this.props
+    const { add, search, description, changeDescription, clear } = this.props
 
     return (
       <div role='form' className='todoForm'>
@@ -42,7 +42,7 @@ class TodoForm extends Component {
         <Grid cols='12 3 2'>
           <IconButton style='primary' icon='plus' onClick={() => add(description)} />
           <IconButton style='info' icon='search' onClick={() => search()} />
-          <IconButton style='default' icon='close' onClick={this.props.handleClear} />
+          <IconButton style='default' icon='close' onClick={() => clear()} />
         </Grid>
       </div>
     )
@@ -50,6 +50,6 @@ class TodoForm extends Component {
 }
 
 const mapStateToProps = state => ({description: state.todo.description})
-const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search, clear }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
